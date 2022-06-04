8a603062faab3be4e5c78369ae5965c9aa3042f3
romain etcheverry <romain.etcheverry@hotmail.fr>
2022-06-04 17:02:45 +0200

    Add dotenv - Protect my secret data in .env file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :show ]
 
   def home
+  end
+
+  def show
   end
 end

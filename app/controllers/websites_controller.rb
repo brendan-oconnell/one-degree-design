e3465766bf78885eadcacd193d07f6f4fe949c97
luc siat <lucsiat@gmail.com>
2022-06-07 20:17:58 +0200

    adding scraping to fetch the font and background

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -28,37 +28,54 @@ class WebsitesController < ApplicationController
     carbon_infos = JSON.parse(carbon)
   end
 
+  def compile_photos_with_cloudinary(html_doc)
+    @photos = []
+    html_doc.search("img").each do |image|
+      query = Cloudinary::Uploader.upload(image.attributes["src"].value)
+      @photos << {
+        bytes: query["bytes"],
+        url: query["url"]
+      }
+    end
+    @photos.sort_by! { |photo| photo[:bytes] }
+    @version.photos = @photos.reverse.first(3)
+
+    # photos_size
+    all_images_size = 0
+    @photos.each { |photo| all_images_size += photo[:bytes] }
+  end
+
   def create_version(website)
+    # if website url does not include https then add it or must match
     url = website.url
     html_file = URI.open(url).read
     html_doc = Nokogiri::HTML(html_file)
 
     # carbon_infos = website_carbon_api(url)
-
     # version = Version.new(website_id: website.id, green_hosting: carbon_infos["green"], bytes: carbon_infos["bytes"], cleaner_than: carbon_infos["cleanerThan"], adjusted_bytes: carbon_infos["statistics"]["adjustedBytes"], energy: carbon_infos["statistics"]["energy"], co2: carbon_infos["statistics"]["co2"]["grid"]["grams"])
     @version = Version.new
-    @photos = {}
 
-    # image Magick
-    html_doc.search("img").each_with_index do |image, index|
-      # regex on src?
-      query = Cloudinary::Uploader.upload(image.attributes["data-src"].value)
-      # image hash
-      @photos[index] = {
-        bytes: query["bytes"],
-        url: query["url"]
-      }
-      puts @version.photos
-    end
-    raise
+    # compile_photos_with_cloudinary(html_doc)
+
 
     # background and font scraping
 
-    # stylesheet = html_doc.search("link")
-    # raise
+    stylesheet_links = []
+    html_doc.search("link").each do |link|
+      stylesheet_links << link.attributes["href"].value if link.attributes["type"].value == "text/css"
+    end
 
+    raise
 
-    # all_images_size, :fonts_file_size, :background_color)
+    style_file = URI.open(stylesheet).read
+
+    style_file.scan(/font-family:[",'].{1,22}[",']/)
+    style_file.scan(/background:.{1,16};/)
+    raise
+
+    # window.getComputedStyle(document.querySelector("h1")).font
+
+    # :fonts_file_size, :background_color)
 
     # save
     # version.save
@@ -69,40 +86,3 @@ class WebsitesController < ApplicationController
   #   params.require(:version).permit(:website_id, :green_hosting, :bytes, :cleaner_than, :adjusted_bytes, :energy, :co2, :all_images_size, :fonts_file_size, :background_color)
   # end
 end
-
-
-# def create_version(website)
-#   url = website.url
-#   html_file = URI.open(url).read
-#   html_doc = Nokogiri::HTML(html_file)
-#   version = Version.new
-
-#   # carbon_infos = website_carbon_api(url)
-
-#   # image Magick
-
-#   array_of_images = []
-#   html_doc.search("img").each_with_index do |image, index|
-#     # Cloudinary::Uploader.upload(image.attributes["src"].value)
-#     file = URI.open(image.attributes["src"].value)
-#     version.photos.attach(io: file, filename: "#{version.id}0#{index}", content_type: 'image/png' )
-#     # array_of_images << cloud_image
-#   end
-#   photo_array = []
-#   version.photos.each do |photo|
-#     photo_array << photo.key
-#   end
-#   raise
-
-#   # background and font scraping
-
-#   # stylesheet = html_doc.search("link")
-#   # raise
-
-
-#   # all_images_size, :fonts_file_size, :background_color)
-
-#   # save
-#   # version(website_id: website.id, green_hosting: carbon_infos["green"], bytes: carbon_infos["bytes"], cleaner_than: carbon_infos["cleanerThan"], adjusted_bytes: carbon_infos["statistics"]["adjustedBytes"], energy: carbon_infos["statistics"]["energy"], co2: carbon_infos["statistics"]["co2"]["grid"]["grams"])
-#   # version.save
-# end

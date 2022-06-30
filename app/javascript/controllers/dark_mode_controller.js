bda1db5b4d9b266d2b805d5eece50d4be9231012
luc siat <lucsiat@gmail.com>
2022-06-30 21:37:04 +0200

    fixes

--- app/javascript/controllers/dark_mode_controller.js
+++ app/javascript/controllers/dark_mode_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["bulb","navbar", "white","green", "footer", "highlight", "image","file","font", "format", "color", "size"]
+  static targets = ["spacer", "bulb","navbar", "white","green", "footer", "highlight", "image","file","font", "format", "color", "size"]
 
   connect() {
     if (localStorage.getItem("darkSwitch") == "light") {
@@ -11,6 +11,7 @@ export default class extends Controller {
       this.greenTarget.classList.toggle("black-background")
       this.footerTarget.classList.toggle("black-background")
       this.highlightTarget.classList.toggle("orange-highlight")
+      this.spacerTarget.classList.toggle("black-background")
       this.imageTarget.classList.toggle("blue-background")
       this.fileTarget.classList.toggle("blue-background")
       this.fontTarget.classList.toggle("blue-background")
@@ -29,6 +30,7 @@ export default class extends Controller {
     this.greenTarget.classList.toggle("black-background")
     this.footerTarget.classList.toggle("black-background")
     this.highlightTarget.classList.toggle("orange-highlight")
+    this.spacerTarget.classList.toggle("black-background")
     this.imageTarget.classList.toggle("blue-background")
     this.fileTarget.classList.toggle("blue-background")
     this.fontTarget.classList.toggle("blue-background")
